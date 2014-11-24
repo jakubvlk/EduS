15925eec0cbf03e5fadbd6d1a698a651a05b05b5
Jakub Vlk <jakvlk@seznam.cz>
2014-11-24 23:45:26 +0100

    creation of project and local database

---
+++ EduS_MVC/App_Data/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[FirstName] NVARCHAR(50) NOT NULL , 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Username] VARCHAR(10) NOT NULL IDENTITY, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [Phone] NVARCHAR(20) NOT NULL, 
+    [BirthDate] DATE NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(200) NOT NULL, 
+    [DegreeBefore] NVARCHAR(50) NOT NULL, 
+    [DegreeAfter] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY ([Username])
+)
---
+++ EduS_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EduS_MVC.MvcApplication" Language="C#" %>
